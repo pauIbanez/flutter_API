e4a7dd93e76524c509af3448778f4c66c3c3ed22
Pau Ibáñez <pauibanez2001@gmail.com>
2022-06-18 16:10:46 +0200

    made the API call for post READ

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_api/models/post.dart';
+import 'package:flutter_api/services/remote_service.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,10 +10,42 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Post>? posts;
+  bool isLoaded = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    getData();
+  }
+
+  getData() async {
+    posts = await RemoteService().getPosts();
+
+    if(posts != null){
+      setState(() {
+        isLoaded = true;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Flutter with API"),),
+      body: Visibility(
+        visible: isLoaded,
+        child: ListView.builder(
+          itemBuilder: (context, index){
+            return Container(
+              child: Text("hi"),
+            );
+          },
+          itemCount: posts?.length,
+        ),
+        replacement: const Center(child: CircularProgressIndicator(),),
+      ),
     );
   }
 }
